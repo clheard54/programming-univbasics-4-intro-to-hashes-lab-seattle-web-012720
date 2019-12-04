fb1aeb0e74b2a5fec93d3422ea3c271954ad33b3
Clarion Heard <clahon54@gmail.com>
2019-12-04 15:19:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,8 +43,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     hash[key] += 1
-  end
-  else !hash.has_key?(key)
+  else
     hash[key] = 1
   end
 end
